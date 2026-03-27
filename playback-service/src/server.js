0d1beddffd3ef3119345a24f225c7b4cbd3d786b
Dulneth Santhuka <dulneth.sa@gmail.com>
2026-03-27 16:32:53 +0530

    feat: Implement initial setup for user, catalog, watchlist, and playback microservices with CI/CD pipeline. And added API documentation using swagger API

--- playback-service/src/server.js
+++ playback-service/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
+const swaggerDocs = require('./config/swagger');
 
 const PORT = process.env.PORT || 5004;
 
@@ -8,5 +9,6 @@ const PORT = process.env.PORT || 5004;
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Playback Service listening on port ${PORT}`);
+    swaggerDocs(app, PORT);
   });
 });
--- user-service/src/server.js
+++ user-service/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
+const swaggerDocs = require('./config/swagger');
 
 const PORT = process.env.PORT || 5001;
 
@@ -8,5 +9,6 @@ const PORT = process.env.PORT || 5001;
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`User Service listening on port ${PORT}`);
+    swaggerDocs(app, PORT);
   });
 });
